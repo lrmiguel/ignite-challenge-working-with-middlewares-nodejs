f5c576c564d2b3956fd7d3872afcc4088bc7c11f
lrmiguel <leo.rmiguel@gmail.com>
2021-09-12 10:26:26 -0300

    feat: :sparkles: Add validation middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,53 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(u => u.username === username)
+  if (!user)
+    return response.status(404).json({ 'error': 'User not found' })
+
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user
+
+  if (user.todos.length >= 10 && !user.pro)
+    return response.status(403).json({ 'error': 'Free plan only allows 10 created todos' })
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const user = users.find(u => u.username === request.headers.username)
+  if (!user)
+    return response.status(404).json({ 'error': 'User not found' })
+
+  const { id } = request.params
+  if (!validate(id))
+    return response.status(400).json()
+
+  const todo = user.todos.find(t => t.id === request.params.id)
+
+  if (!todo)
+    return response.status(404).json({ 'error': 'Todo not found' })
+
+  request.user = user
+  request.todo = todo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const user = users.find(u => u.id === id)
+  if (!user)
+    return response.status(404).json({ 'error': 'User not found' })
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
